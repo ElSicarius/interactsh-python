5f2e0355d3a6ae3d12ed05a070b8a15dc911b692
ElSicarius <amurat654@gmail.com>
2022-08-25 14:36:28 +0200

    Update with context manager, code beautified and new list of servers

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ A tool to use interactsh from python for automating OOB things
 
 ```python
 >>> from sources.interactsh import Interactsh
->>> a = Interactsh()
->>> a.get_handle()
+>>> with Interactsh() as handle
+>>>     a.get_handle()
 'http://7p6124c7562v4035s95y8k5p085073fhs.interact.sh/'
 >>> import requests
->>> requests.get(a.get_handle())
->>> a.poll()
+>>>     requests.get(a.get_handle())
+>>>     a.poll()
 [{'timestamp': '2022-06-09T12:32:38.387891517Z', 'host': '7p6124c7562v4035s95y8k5p085073fhs.7p6124c7562v4035s95y8k5p085073fhs.interact.sh', 'remote_address': 'xxx', 'raw-request': ';; opcode: QUERY, status: NOERROR, id: 32453\n;; flags: cd; QUERY: 1, ANSWER: 0, AUTHORITY: 0, ADDITIONAL: 1\n\n;; OPT PSEUDOSECTION:\n; EDNS: version 0; flags: do; udp: 512\n\n;; QUESTION SECTION:\n;7p6124c7562v4035s95y8k5p085073fhs.interact.sh.\tIN\t AAAA\n', 'protocol': 'dns'}, {'timestamp': '2022-06-09T12:32:38.410108026Z', 'host': '7p6124c7562v4035s95y8k5p085073fhs.7p6124c7562v4035s95y8k5p085073fhs.interact.sh', 'remote_address': 'xxxx', 'raw-request': ';; opcode: QUERY, status: NOERROR, id: 34065\n;; flags: cd; QUERY: 1, ANSWER: 0, AUTHORITY: 0, ADDITIONAL: 1\n\n;; OPT PSEUDOSECTION:\n; EDNS: version 0; flags: do; udp: 512\n\n;; QUESTION SECTION:\n;7p6124c7562v4035s95y8k5p085073fhs.interact.sh.\tIN\t AAAA\n', 'protocol': 'dns'}, {'timestamp': '2022-06-09T12:32:38.429969193Z', 'host': '7p6124c7562v4035s95y8k5p085073fhs.7p6124c7562v4035s95y8k5p085073fhs.interact.sh', 'remote_address': 'xxxxx', 'raw-request': .........
->>> a.deregister()
 ```
+
+By using the context manager, the handle is automatically deregistered once the context is ended.
--- sources/interactsh.py
+++ sources/interactsh.py
@@ -9,8 +9,30 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 
+
 class Interactsh:
-    def __init__(self, token=None, servers=["interact.sh", "interactsh.com"]):
+    public_key: str
+    private_key: str
+    token: str
+    servers: list
+    headers: dict
+    secret: str
+    guid: str
+    correlation_id: str
+    session: requests.Session
+
+    def __init__(
+        self,
+        token=None,
+        servers=[
+            "oast.me",
+            "oast.fun",
+            "oast.site",
+            "oast.pro",
+            "oast.live",
+            "oast.online",
+        ],
+    ):
         rsa = RSA.generate(1024)
         self.public_key = rsa.publickey().exportKey()
         self.private_key = rsa.exportKey()
@@ -20,87 +42,109 @@ class Interactsh:
             "Content-Type": "application/json",
         }
         if self.token:
-            self.headers['Authorization'] = self.token
+            self.headers["Authorization"] = self.token
         self.secret = str(uuid4())
         self.encoded = base64.b64encode(self.public_key).decode("utf8")
-        guid = uuid4().hex.ljust(33, 'a')
-        self.guid = ''.join(i if i.isdigit() else chr(ord(i) + random.randint(0, 20)) for i in guid)
+        guid = uuid4().hex.ljust(33, "a")
+        self.guid = "".join(
+            i if i.isdigit() else chr(ord(i) + random.randint(0, 20)) for i in guid
+        )
 
         self.correlation_id = self.guid[:20]
 
         self.session = requests.session()
         self.session.headers = self.headers
+    
+    def __enter__(self):
         self.register()
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.deregister()
 
-    def deregister(self):
+    def deregister(self) -> None:
         """
         : deregister token
         """
-        data = {
-            "secret-key": self.secret,
-            "correlation-id": self.correlation_id
-        }
+        data = {"secret-key": self.secret, "correlation-id": self.correlation_id}
         try:
-            r = requests.post(f"https://{self.server}/deregister", json=data, headers=self.headers, timeout=1, verify=False)
+            requests.post(
+                f"https://{self.server}/deregister",
+                json=data,
+                headers=self.headers,
+                timeout=1,
+                verify=False,
+            )
         except Exception as e:
             logger.error(e)
-            return
-        #print(f"Interactsh deregister response {r.text}")
 
-    def register(self):
+    def register(self) -> None:
         data = {
             "public-key": self.encoded,
             "secret-key": self.secret,
-            "correlation-id": self.correlation_id
+            "correlation-id": self.correlation_id,
         }
-        #print(.debug(f"Registering \nKey: {self.encoded}\nSecret: {self.secret}\nHandle: {self.correlation_id}")
         for server in self.servers:
             try:
                 res = self.session.post(
-                    f"https://{server}/register", headers=self.headers, json=data, timeout=10, verify=False)
+                    f"https://{server}/register",
+                    headers=self.headers,
+                    json=data,
+                    timeout=10,
+                    verify=False,
+                )
                 self.server = server
-                self.domain = f'{self.guid}.{self.server}'
+                self.domain = f"{self.guid}.{self.server}"
                 # print(.debug(self.domain)
                 break
             except Exception as e:
-                logger.error(f"Server {server} failed to register, trying the next one: {e}")
+                logger.error(
+                    f"Server {server} failed to register, trying the next one: {e}"
+                )
                 continue
 
-        if 'success' not in res.text:
+        if "success" not in res.text:
             logger.error(res.text)
 
         self.write_handle_logs()
 
     def write_handle_logs(self):
+        # TODO
         pass
 
-    def poll(self):
+    def poll(self) -> list:
         result = []
-        url = f"https://{self.server}/poll?id={self.correlation_id}&secret={self.secret}"
+        url = (
+            f"https://{self.server}/poll?id={self.correlation_id}&secret={self.secret}"
+        )
         res = self.session.get(url, headers=self.headers, verify=False, timeout=30)
         try:
             res = res.json()
         except:
-            logger.error('An error occured with the target, maybe a proxy is blocking you ?')
+            logger.error(
+                "An error occured with the target, maybe a proxy is blocking you ?"
+            )
             logger.debug(res.text)
             return None
-        aes_key, data_list = res['aes_key'], res['data']
-        #print(.debug(res)
+
+        aes_key, data_list = res["aes_key"], res["data"]
+
         for i in data_list:
             decrypt_data = self.decrypt_data(aes_key, i)
             result.append(self.__parse_log(decrypt_data))
         return result
 
-    def __parse_log(self, log_entry):
-        new_log_entry = {"timestamp": log_entry["timestamp"],
-                         "host": f'{log_entry["full-id"]}.{self.domain}',
-                         "remote_address": log_entry["remote-address"],
-                         "raw-request": log_entry["raw-request"],
-                         "protocol": log_entry["protocol"]
-                         }
+    def __parse_log(self, log_entry: dict) -> dict:
+        new_log_entry = {
+            "timestamp": log_entry["timestamp"],
+            "host": f'{log_entry["full-id"]}.{self.domain}',
+            "remote_address": log_entry["remote-address"],
+            "raw-request": log_entry["raw-request"],
+            "protocol": log_entry["protocol"],
+        }
         return new_log_entry
 
-    def decrypt_data(self, aes_key, data):
+    def decrypt_data(self, aes_key: str, data: str) -> dict:
         private_key = RSA.importKey(self.private_key)
         cipher = PKCS1_OAEP.new(private_key, hashAlgo=SHA256)
         aes_plain_key = cipher.decrypt(base64.b64decode(aes_key))
@@ -109,7 +153,8 @@ class Interactsh:
         iv = decode[:bs]
         cryptor = AES.new(key=aes_plain_key, mode=AES.MODE_CFB, IV=iv, segment_size=128)
         plain_text = cryptor.decrypt(decode)
+
         return json.loads(plain_text[16:])
 
-    def get_handle(self):
+    def get_handle(self) -> str:
         return f"http://{self.domain}/"
